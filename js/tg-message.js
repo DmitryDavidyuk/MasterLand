52c20aea8514f537f63da4959db0c8c8fdcb7770
Dmitry Davidyk <91990771+DmitryDavidyuk@users.noreply.github.com>
2023-02-01 23:43:06 +0200

    і

--- js/tg-message.js
+++ js/tg-message.js
@@ -1,16 +1,15 @@
 const TOKEN = '5977675073:AAGRZsFwWksh60DfRFB9lV5iLRPzee8r1fM';
-      const CHAT_ID = '-1001790959821';
-      const URL = `https://api.telegram.org/bot${TOKEN}/SendMessage`;
-      const modal = document.querySelector("[data-modal]");
+const CHAT_ID = '-1001790959821';
+const URL = `https://api.telegram.org/bot${TOKEN}/SendMessage`;
+const modal = document.querySelector("[data-modal]");
 
 document.getElementById('tg-form').addEventListener('submit', function(e) {
     e.preventDefault();
-    console.log(e);
+
     let message = `<b>Заявка з сайта</b>\n`;
     message += `<b>Ім'я </b> ${this.name.value}\n`;
-    message += `<b>Email </b> ${this.email.value}\n`;
     message += `<b>Phone </b> ${this.tel.value}\n`;
-    console.log(message);
+    message += `<b>Email </b> ${this.email.value}\n`;
 
     axios.post(URL, {
         chat_id: CHAT_ID,
